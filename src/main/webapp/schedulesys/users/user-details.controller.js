23e13911eeb307d31927eb8738504ff139b60363
ethral <abhinavsudam1@gmail.com>
2016-08-14 21:46:28 -0400

    users.details

--- src/main/webapp/schedulesys/users/user-details.controller.js
+++ src/main/webapp/schedulesys/users/user-details.controller.js
@@ -2,57 +2,29 @@
 	'use strict';
 	angular
 		.module('scheduleSys')
-		.controller('userDialogController', userDialogController);
+		.controller('userDetailsController', userDetailsController);
 	
-	userDialogController.$Inject = ['$state', '$mdDialog', '$mdToast', 'usersService','userRoleService'];
+	userDetailsController.$Inject = ['$state', '$stateParams', '$mdDialog',
+	                                     '$mdToast', 'usersService'];
 	
-	function userDialogController($state, $mdDialog, $mdToast, usersService,userRoleService){
+	function userDetailsController($state, $stateParams, $mdDialog, $mdToast, usersService){
 		var vm = this;
 		
-		vm.cancel = cancel;
-		vm.createOrUpdateuser = createOrUpdateuser;
-		vm.showToast = showToast;
-		vm.userRole = userRoleService.query();
-		vm.options6 = vm.userRole;
+		vm.getSelectedUser = getSelectedUser;
 		
-		vm.myModel = {};
-		vm.user = {
-				id: null,
-				username: null,
-				userRole: null
-			
+		vm.selected = [];
+		vm.query = {
+				limit: 5,
+				page: 1	
 		};
 		
-		function cancel() {
-			$mdDialog.cancel();
-		}
 		
-		function createOrUpdateuser(){
-			console.log('User to be created : ' + angular.toJson(vm.user));
-			if(vm.user.id === null){
-				usersService.save(vm.user, onCreateOrUpdateSucess, onCreateOrUpdateFailure);
-			}else{
-				usersService.update(vm.user, onCreateOrUpdateSucess, onCreateOrUpdateFailure);
-			}
-		}
+		getSelectedUser();
 		
-		function onCreateOrUpdateSucess(result){
-			$state.go('home.users',{}, {reload: true});
-			$mdDialog.cancel();
-			vm.showToast('User ' + vm.user.username + ' successfully created', 5000);
-		}
-		
-		function onCreateOrUpdateFailure(result){
-			vm.showToast(result.data, 5000);
-		}
-		
-		function showToast(textContent, delay){
-			$mdToast.show(
-					$mdToast.simple()
-					.textContent(textContent)
-					.position('top right')
-					.hideDelay(delay));
+		function getSelectedUser(){
+			usersService.get({id:$stateParams.id}, function(result) {
+				vm.user = result;
+			})
 		}
 	}
-	
 })();
