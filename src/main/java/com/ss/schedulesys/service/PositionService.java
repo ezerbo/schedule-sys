b9a4ad94d15c38aa83dd33441b3c61fc643be9d9
ezerbo <zerboedouard@gmail.com>
2017-12-23 16:56:39 -0500

    Add Position Management UI

--- src/main/java/com/ss/schedulesys/service/PositionService.java
+++ src/main/java/com/ss/schedulesys/service/PositionService.java
@@ -1,6 +1,7 @@
 package com.ss.schedulesys.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,11 +9,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.ss.schedulesys.domain.Position;
 import com.ss.schedulesys.repository.PositionRepository;
+import com.ss.schedulesys.service.errors.ScheduleSysException;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@Transactional
 public class PositionService {
 
 	private PositionRepository positionRepository;
@@ -22,10 +25,34 @@ public class PositionService {
 		this.positionRepository = positionRepository;
 	}
 	
+	public Position save(Position position) {
+		log.debug("Creating or Updating position : {}", position);
+		Position existingPosition = positionRepository.findByName(position.getName());
+		if(existingPosition != null && (position.getId() != existingPosition.getId())) {
+			log.error("Position name '{}' already in use", position.getName());
+			throw new ScheduleSysException(String.format("Position name '%s' already in use", position.getName()));
+		}
+		position = positionRepository.save(position);
+		return position;
+	}
+	
+	
 	@Transactional(readOnly = true)
 	public List<Position> getAll(){
 		log.debug("Fetching all positions");
 		List<Position> positions = positionRepository.findAll();
 		return positions;
+	}
+	
+	public void delete(Long id) {
+		log.debug("Deleting position with Id : {}", id);
+		Position position = Optional.ofNullable(positionRepository.getOne(id))
+				.orElseThrow(() -> new ScheduleSysException(String.format("No position found with Id : '%s'", id)));
+		if(position.getEmployees().isEmpty()){
+			positionRepository.delete(id);
+		} else {
+			throw new ScheduleSysException(String.format("Position '%s' is in use",position.getName()));
+		}
+		
 	}
 }
--- src/main/java/com/ss/schedulesys/web/rest/PositionResource.java
+++ src/main/java/com/ss/schedulesys/web/rest/PositionResource.java
@@ -1,17 +1,26 @@
 package com.ss.schedulesys.web.rest;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ss.schedulesys.domain.Position;
 import com.ss.schedulesys.service.PositionService;
+import com.ss.schedulesys.web.rest.util.HeaderUtil;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,11 +39,64 @@ public class PositionResource {
 		this.positionService = positionService;
 	}
 	
+	 /**
+     * POST  /positions : Create a new position.
+     *
+     * @param position the position to create
+     * @return the ResponseEntity with status 201 (Created) and with body the new position, or with status 400 (Bad Request) if the position has already an ID
+     * @throws URISyntaxException if the Location URI syntax is incorrect
+     */
+    @PostMapping("/positions")
+    public ResponseEntity<Position> createPosition(@Valid @RequestBody Position position) throws URISyntaxException {
+        log.info("REST request to save Position : {}", position);
+        if (position.getId() != null) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("position", "idexists", "A new position cannot already have an ID")).body(null);
+        }
+        Position result = positionService.save(position);
+        return ResponseEntity.created(new URI("/api/positions/" + result.getId()))
+            .headers(HeaderUtil.createEntityCreationAlert("position", result.getId().toString()))
+            .body(result);
+    }
+
+    /**
+     * PUT  /positions : Updates an existing position.
+     *
+     * @param position the position to update
+     * @return the ResponseEntity with status 200 (OK) and with body the updated position,
+     * or with status 400 (Bad Request) if the position is not valid,
+     * or with status 500 (Internal Server Error) if the position couldn't be updated
+     * @throws URISyntaxException if the Location URI syntax is incorrect
+     */
+    @PutMapping("/positions")
+    public ResponseEntity<Position> updatePosition(@Valid @RequestBody Position position) throws URISyntaxException {
+        log.debug("REST request to update Position : {}", position);
+        if (position.getId() == null) {
+            return createPosition(position);
+        }
+        Position result = positionService.save(position);
+        return ResponseEntity.ok()
+            .headers(HeaderUtil.createEntityUpdateAlert("position", position.getId().toString()))
+            .body(result);
+    }
+    
+    /**
+     * DELETE  /positions/:id : delete the "id" position.
+     *
+     * @param id the id of the position to delete
+     * @return the ResponseEntity with status 200 (OK)
+     */
+    @DeleteMapping("/positions/{id}")
+    public ResponseEntity<Void> deleteLicense(@PathVariable Long id) {
+        log.debug("REST request to delete Position : {}", id);
+        positionService.delete(id);
+        return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("position", id.toString())).build();
+    }
+	
 	/**
     * GET  /positions : get all the positions.
     *
     * @param pageable the pagination information
-    * @return the ResponseEntity with status 200 (OK) and the list of licenses in body
+    * @return the ResponseEntity with status 200 (OK) and the list of positions in body
     */
     @GetMapping("/positions")
     public ResponseEntity<List<Position>> getAllPositions()
