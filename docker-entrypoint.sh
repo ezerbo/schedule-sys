73e30d2007f6ea59e395990948841d1b82987f30
ezerbo <zerboedouard@gmail.com>
2018-12-02 00:25:36 -0500

    Add remote debugging argument to jvm

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,5 +14,8 @@ if [ -n "$EMAIL_PORT" ]; then
    SMTP_HOST="email"
 fi
 echo "Running with Profile:" $APP_PROFILE "SMTP host:" $SMTP_HOST
-java -jar schedule-sys.war --spring.profiles.active=$APP_PROFILE --spring.mail.host=$SMTP_HOST
+java -Djava.security.egd=file:/dev/./urandom \
+     -Xdebug \
+     -Xrunjdwp:server=y,address=8000,transport=dt_socket,suspend=n \
+     -jar schedule-sys.war --spring.profiles.active=$APP_PROFILE --spring.mail.host=$SMTP_HOST
 exec "$@"
